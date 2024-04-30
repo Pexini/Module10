225b3b1fc0f94229272178acf8260577f7fb7f7f
Pavel Davydov <Davydov.works@gmail.com>
2024-04-30 01:41:41 -0400

    reformat TaskList

--- src/main/java/TaskList/DocumentTask.java
+++ src/main/java/TaskList/DocumentTask.java
@@ -1,4 +1,25 @@
 package TaskList;
 
-public class DocumentTask {
+public class DocumentTask implements Task{
+    private String documentLink;
+    private boolean finished;
+
+    public DocumentTask(String documentLink) {
+        this.documentLink = documentLink;
+    }
+
+    @Override
+    public void showTask() {
+        System.out.println("Отображаем документ. Ссылка на документ " + documentLink);
+    }
+
+    @Override
+    public void executed() {
+        finished = finished;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
 }
