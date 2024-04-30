9ed0ba047bd4254fe16f5307f057a54dd5aafe05
Pavel Davydov <Davydov.works@gmail.com>
2024-04-30 01:48:20 -0400

    reformat TaskList

---
+++ src/main/java/TaskList/TaskList.java
@@ -0,0 +1,28 @@
+package TaskList;
+
+public class TaskList {
+    private Task[] tasks;
+
+    public TaskList(Task[] tasks) {
+        this.tasks = tasks;
+    }
+
+    public void showFinishedTask() {
+        System.out.println("Завершенные задачи");
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i].isFinished()) {
+                tasks[i].showTask();
+            }
+        }
+    }
+
+    public void showUnFinishedTask() {
+        System.out.println("Незавершенные дела");
+        for (int i = 0; i < tasks.length; i++) {
+            if (!tasks[i].isFinished()) {
+                tasks[i].showTask();
+            }
+        }
+    }
+
+}
