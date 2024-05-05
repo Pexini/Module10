e6a84599b279ee762a1954a9d8d4c6f02f086cd0
Pavel Davydov <Davydov.works@gmail.com>
2024-05-05 03:11:36 -0400

    Weapon

--- src/main/java/Hard/WorldBattle/Weapons/WeaponsFactory.java
+++ src/main/java/Hard/WorldBattle/Weapons/WeaponsFactory.java
@@ -1,6 +1,7 @@
 package Hard.WorldBattle.Weapons;
 
 import Hard.WorldBattle.Characters.Hero.Detective;
+import Hard.WorldBattle.Characters.Hero.Librarian;
 
 import javax.xml.stream.events.Characters;
 
@@ -8,6 +9,7 @@ public class WeaponsFactory {
     public static Weapon getWeapon(TypeOfWeapons typeOfWeapons) {
         if (Detective.equals(typeOfHero)) {
             return new revolver;
+            if (Librarian.equals(type))
         }
     }
 }
